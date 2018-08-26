603d1976d6c3fde64fb6357dbff38df9588396ff
Kerim Erçin Deveci <kerim3499@gmail.com>
2018-08-26 15:13:35 +0300

    Slider Project Complete

--- Projects/Slider/Form1.Designer.cs
+++ Projects/Slider/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Slider
 {
-    partial class Form1
+    partial class frmSlider
     {
         /// <summary>
         ///Gerekli tasarımcı değişkeni.
@@ -28,38 +28,77 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.lblLogin = new System.Windows.Forms.Label();
+            this.lblSignUp = new System.Windows.Forms.Label();
+            this.pnlLine = new System.Windows.Forms.Panel();
+            this.pnlSelect = new System.Windows.Forms.Panel();
             this.SuspendLayout();
             // 
-            // pictureBox1
+            // lblLogin
             // 
-            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Left;
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(349, 395);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            this.lblLogin.AutoSize = true;
+            this.lblLogin.Font = new System.Drawing.Font("Roboto Condensed", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblLogin.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblLogin.Location = new System.Drawing.Point(12, 9);
+            this.lblLogin.Name = "lblLogin";
+            this.lblLogin.Size = new System.Drawing.Size(52, 23);
+            this.lblLogin.TabIndex = 0;
+            this.lblLogin.Text = "Login";
+            this.lblLogin.Click += new System.EventHandler(this.lblLogin_Click);
             // 
-            // Form1
+            // lblSignUp
+            // 
+            this.lblSignUp.AutoSize = true;
+            this.lblSignUp.Font = new System.Drawing.Font("Roboto Condensed", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSignUp.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblSignUp.Location = new System.Drawing.Point(73, 9);
+            this.lblSignUp.Name = "lblSignUp";
+            this.lblSignUp.Size = new System.Drawing.Size(66, 23);
+            this.lblSignUp.TabIndex = 1;
+            this.lblSignUp.Text = "Sign Up";
+            this.lblSignUp.Click += new System.EventHandler(this.lblSignUp_Click);
+            // 
+            // pnlLine
+            // 
+            this.pnlLine.BackColor = System.Drawing.SystemColors.Control;
+            this.pnlLine.ForeColor = System.Drawing.SystemColors.Control;
+            this.pnlLine.Location = new System.Drawing.Point(-1, 37);
+            this.pnlLine.Name = "pnlLine";
+            this.pnlLine.Size = new System.Drawing.Size(354, 1);
+            this.pnlLine.TabIndex = 2;
+            // 
+            // pnlSelect
+            // 
+            this.pnlSelect.BackColor = System.Drawing.SystemColors.Control;
+            this.pnlSelect.Location = new System.Drawing.Point(10, 33);
+            this.pnlSelect.Name = "pnlSelect";
+            this.pnlSelect.Size = new System.Drawing.Size(57, 8);
+            this.pnlSelect.TabIndex = 3;
+            // 
+            // frmSlider
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(692, 395);
-            this.Controls.Add(this.pictureBox1);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(12)))), ((int)(((byte)(28)))));
+            this.ClientSize = new System.Drawing.Size(351, 395);
+            this.Controls.Add(this.pnlSelect);
+            this.Controls.Add(this.pnlLine);
+            this.Controls.Add(this.lblSignUp);
+            this.Controls.Add(this.lblLogin);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Name = "Form1";
-            this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.Name = "frmSlider";
+            this.Load += new System.EventHandler(this.frmSlider_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblLogin;
+        private System.Windows.Forms.Label lblSignUp;
+        private System.Windows.Forms.Panel pnlLine;
+        private System.Windows.Forms.Panel pnlSelect;
     }
 }
 
--- Projects/Slider/Form1.cs
+++ Projects/Slider/Form1.cs
@@ -10,11 +10,44 @@ using System.Windows.Forms;
 
 namespace Slider
 {
-    public partial class Form1 : Form
+    public partial class frmSlider : Form
     {
-        public Form1()
+        public frmSlider()
         {
             InitializeComponent();
+        }
+
+        private void frmSlider_Load(object sender, EventArgs e)
+        {
+            lblLogin_Click(sender, e);
+        }
+
+        private void lblLogin_Click(object sender, EventArgs e)
+        {
+            Slider(lblLogin.Name, lblLogin.Width, lblLogin.Left);
+        }
+
+        private void lblSignUp_Click(object sender, EventArgs e)
+        {
+            Slider(lblSignUp.Name, lblSignUp.Width, lblSignUp.Left);
+        }
+
+        public void Slider(string lbl, int widht, int left)
+        {
+            if (lbl == "lblLogin")
+            {
+                pnlSelect.Left = left;
+                pnlSelect.Width = widht;
+                lblLogin.Font = new Font(lblLogin.Font, FontStyle.Bold);
+                lblSignUp.Font = new Font(lblSignUp.Font, FontStyle.Regular);
+            }
+            else
+            {
+                pnlSelect.Left = left;
+                pnlSelect.Width = widht;
+                lblSignUp.Font = new Font(lblSignUp.Font, FontStyle.Bold);
+                lblLogin.Font = new Font(lblLogin.Font, FontStyle.Regular);
+            }
         }
     }
 }
